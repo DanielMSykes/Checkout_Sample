5bc0be43b75d14359153edf9a210a42f3ddf5bd5
Daniel M Sykes <d.m.sykes@icloud.com>
2022-05-15 17:01:07 +0100

    Added discount to Product B
    3 for 40

--- Checkout_Sample/Shop.cs
+++ Checkout_Sample/Shop.cs
@@ -17,6 +17,30 @@ namespace Checkout_Sample
             {typeof(Products.D), 55m },
         };
 
+        /*Introduce discounts*/
+        public static Dictionary<Type, Func<List<Product>, decimal>> Discounts = new Dictionary<Type, Func<List<Product>, decimal>>()
+        {
+            {typeof(Products.B) sku => {
+                var multipackB = sku.Where(s => s.GetType() == typeof(Products.B)).ToList();
+                var count = multipackB.Count;
+
+                if (count > 0)
+                {
+                    var price = multipackB[0].Price;
+                    var discountPrice = Math.Floor((count / 3.0m) * 5);
+
+                    return (price - discountPrice);
+                }
+                else
+                {
+                    return 0m;
+                }
+            }},
+            {typeof(Products.D) sku => {
+            
+            }}
+        };
+
         /*Return the actual price of products*/
         public static Product? GetProduct(Type product)
         {
@@ -31,7 +55,5 @@ namespace Checkout_Sample
                 throw new ArgumentOutOfRangeException("SKU does not exist");
             }
         }
-
-        /**/
     }
 }
